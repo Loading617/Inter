3e4969f498cc8e8c944ffc706282d69af92deb10
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-05 21:17:48 -0500

    Delete signalmessagetype.rs

--- signalmessagetype.rs
+++
@@ -1,7 +0,0 @@
-#[derive(Serialize, Deserialize)]
-pub struct SignalingMessage {
-    pub type_: String,       // "offer", "answer", or "candidate"
-    pub sdp: Option<String>, // Session Description Protocol
-    pub candidate: Option<String>,
-    pub recipient_id: i32,
-}
