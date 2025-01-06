25a2d5b932176c1007a6c61205fff15c42b15f02
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-05 21:17:24 -0500

    Delete messagepersist.sql

--- messagepersist.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE messages (
-    id SERIAL PRIMARY KEY,
-    sender_id INT NOT NULL,
-    recipient_id INT NOT NULL,
-    content TEXT NOT NULL,
-    timestamp TIMESTAMP DEFAULT NOW()
-);
