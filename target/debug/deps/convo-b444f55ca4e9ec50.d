a0a0e4f78c47c14a986abcb1fb14d35cf1ef46d5
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-06 12:03:41 -0500

    Delete target directory

--- target/debug/deps/convo-b444f55ca4e9ec50.d
+++
@@ -1,5 +0,0 @@
-C:\Convo\convo\target\debug\deps\libconvo-b444f55ca4e9ec50.rmeta: src\main.rs
-
-C:\Convo\convo\target\debug\deps\convo-b444f55ca4e9ec50.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/convo-dd9ec2942096759b.d
+++
@@ -1,5 +0,0 @@
-C:\Convo\convo\target\debug\deps\libconvo-dd9ec2942096759b.rmeta: src\main.rs
-
-C:\Convo\convo\target\debug\deps\convo-dd9ec2942096759b.d: src\main.rs
-
-src\main.rs:
