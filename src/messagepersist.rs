81ef5b3ec8451b9d2a3b44d932b5d27277e45a6e
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-06 12:03:28 -0500

    Delete src directory

--- src/messagepersist.rs
+++
@@ -1,8 +0,0 @@
-#[derive(Debug, Queryable)]
-pub struct Message {
-    pub id: i32,
-    pub sender_id: i32,
-    pub recipient_id: i32,
-    pub content: String,
-    pub timestamp: NaiveDateTime,
-}
--- src/test.rs
+++
@@ -1,5 +0,0 @@
-#[tokio::test]
-async fn test_send_message() {
-    let response = send_message(MockRequest::new(payload)).await;
-    assert_eq!(response.status(), StatusCode::OK);
-}
