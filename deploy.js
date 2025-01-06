7c281a1f8402eb8f462248e21ffc3788436f1e2a
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-05 21:16:43 -0500

    Delete deploy.js

--- deploy.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    iceServers: [
-      { urls: "stun:stun.l.google.com:19302" }, // Free STUN server
-      { urls: "turn:your-turn-server.com", username: "user", credential: "pass" },
-    ],
-  };
-  const peerConnection = new RTCPeerConnection(config);
-  
