abd192217e09657d935f098bc0ceb54c56df14fd
Jahim Sankoh <jahimsankoh319@gmail.com>
2025-01-08 10:34:51 -0500

    Delete target directory

--- target/debug/deps/fltk_convo-959e6da32d24cdfb.d
+++
@@ -1,5 +0,0 @@
-C:\Convo\fltk_convo\target\debug\deps\libfltk_convo-959e6da32d24cdfb.rmeta: src\main.rs
-
-C:\Convo\fltk_convo\target\debug\deps\fltk_convo-959e6da32d24cdfb.d: src\main.rs
-
-src\main.rs:
--- target/debug/deps/fltk_convo-9924b122dc0ec200.d
+++
@@ -1,5 +0,0 @@
-C:\Convo\fltk_convo\target\debug\deps\libfltk_convo-9924b122dc0ec200.rmeta: src\main.rs
-
-C:\Convo\fltk_convo\target\debug\deps\fltk_convo-9924b122dc0ec200.d: src\main.rs
-
-src\main.rs:
